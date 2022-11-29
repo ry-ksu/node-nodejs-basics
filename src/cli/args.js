2505aa66e0498af2739066840201cdcf560d1c3f
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-11-29 08:17:46 +0300

    feat: add solution to cli folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args.reduce((accumulator, curValue, curIndex) => {
+    if (curValue.startsWith("--")) {
+      accumulator.push(`${curValue.slice(2)} is ${args[curIndex + 1]}`)
+    }
+
+    return accumulator;
+  }, []);
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const RSSKeysValues = Object.entries(process.env)
+    .reduce((accumulator, [key, value]) => {
+      if (key.startsWith("RSS_")) {
+        accumulator.push(`${key}=${value}`)
+      }
+      return accumulator
+    }, []);
+  
+    console.log(RSSKeysValues.join('; '));
 };
 
 parseEnv();
