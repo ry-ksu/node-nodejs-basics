fcb97014cad0b154b9aff4227b433aa9ebd60803
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-11-30 19:35:23 +0300

    feat: add solution to hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+
+  const input = createReadStream(filePath);
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
