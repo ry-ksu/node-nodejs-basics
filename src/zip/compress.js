f8862fef1829c68f47a7f548a16a6671f541a5bb
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-12-05 20:33:37 +0300

    feat: add solution to zip folder

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { promisify } from 'util';
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const sourcePath = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+  const destinationPath = path.resolve(__dirname, 'files', 'archive.gz');
+
+  const pipe = promisify(pipeline);
+
+  const doGzip = async (input, output) => {
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, gzip, destination);
+  }
+
+  try {
+    doGzip(sourcePath, destinationPath)
+  } catch(err) {
+    throw new Error(err);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { promisify } from 'util';
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const source = path.resolve(__dirname, 'files', 'archive.gz');
+  const destination = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+
+  const pipe = promisify(pipeline);
+
+  const doGunzip = async (input, output) => {
+    const unGzip = createGunzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, unGzip, destination);
+  }
+
+  try {
+    await doGunzip(source,destination)
+  } catch(err) {
+    throw new Error(err);
+  }
 };
 
 await decompress();
